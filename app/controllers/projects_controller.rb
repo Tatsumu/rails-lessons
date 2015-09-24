fa16b0d8306b5b3de8755e0e0f19ecbedce6b0e8
tatsumu <t.m.555.tennis@gmail.com>
2015-09-24 09:56:59 +0900

    lesson12

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,12 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.all
 	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	def new
+		@project = Project.new
+	end
 end
