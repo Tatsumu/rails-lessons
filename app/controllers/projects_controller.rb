1e47ef5e128bbd6fdcd0e7061ee0e06acb403898
tatsumu <t.m.555.tennis@gmail.com>
2015-09-25 13:57:29 +0900

    lesson17

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
-	def index
+
+	def index#rake routesのうち行いたいアクションを定義する
 		@projects = Project.all
 	end
 
@@ -10,4 +11,31 @@ class ProjectsController < ApplicationController
 	def new
 		@project = Project.new
 	end
+
+	def create 
+		@project = Project.new(project_params)
+		if @project.save #true validationに引っかからなかったら
+		redirect_to projects_path
+		else #false　引っかかったら
+			render 'new'#newのviewに戻る
+		end
+	end
+
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])
+		if @project.update(project_params)
+			redirect_to projects_path
+		else
+			render 'edit'
+		end
+	end
+
+	private #フィルタリングのために必要
+		def project_params
+			params[:project].permit(:title)
+		end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+	validates :title,
+	presence: { message: "入力してください"}, #validationでtitleがnilの場合拒否する
+	length: { minimum: 3, message: "短けぇぞコラァ！"}
 end
