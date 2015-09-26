2a3d32b02bcd3584dad37c5f9911403fb32cf568
tatsumu <t.m.555.tennis@gmail.com>
2015-09-26 13:58:57 +0900

    lesson25

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+	def create 
+		@project = Project.find(params[:project_id])#ターミナルでrake routesで出てくる:project_idを引っ張ってくる
+		@task = @project.tasks.create(task_params)
+		redirect_to project_path(@project.id)	
+	end
+
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+		redirect_to projects_path(params[:project_id])
+	end	
+
+	private #フィルタリングのために必要
+		def task_params
+			params[:task].permit(:title)
+		end
 end
